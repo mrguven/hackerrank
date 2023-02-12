b36eabfb1548d246e54accb70d89679c468ef58d
mrguven <mrguven@gmail.com>
2023-02-12 16:27:43 +0100

    update

---
+++ HackYourFuture.js
@@ -0,0 +1,11 @@
+function giveCompliment(/* TODO parameter(s) go here */) {
+    // TODO complete this function
+
+let compliment = ["great","awesome","super","good job","ok","hi","tr","bad","super","go on"]
+
+
+
+
+  }
+  
+  
