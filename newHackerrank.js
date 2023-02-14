508e1f3a1c1e604e833e455c0390611a49808305
mrguven <mrguven@gmail.com>
2023-02-14 11:03:18 +0100

    update

--- newHackerrank.js
+++ newHackerrank.js
@@ -379,3 +379,43 @@ married to ${selectRondomly (partnerNames)} with ${selectRondomly (numKids)} kid
 
 console.log(tellFortune(numKids,partnerNames,locations,jobTitles));
 console.log(tellFortune(numKids,partnerNames,locations,jobTitles));
+
+
+//----------------------
+
+
+const mondayTasks = [
+    {
+      name: 'Daily standup',
+      duration: 30, // specified in minutes
+    },
+    {
+      name: 'Feature discussion',
+      duration: 120,
+    },
+    {
+      name: 'Development time',
+      duration: 240,
+    },
+    {
+      name: 'Talk to different members from the product team',
+      duration: 60,
+    },
+  ];
+
+  const hourlyRate = 25;
+
+
+  function computeEarnings(hourlyRate,mondayTasks) {
+    // TODO complete this function
+let totalPrice =0;
+    for (let i=0;i<mondayTasks.length;i++) {
+        
+        totalPrice+= mondayTasks[i].duration/60 * hourlyRate;
+    }
+
+
+return totalPrice;
+  }
+
+  console.log(computeEarnings(hourlyRate,mondayTasks))
