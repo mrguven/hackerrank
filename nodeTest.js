58b4220f258ac92775cf7f7b129cff173b55c7e4
mrguven <mrguven@gmail.com>
2023-03-17 23:13:21 +0100

    updeta

--- nodeTest.js
+++ nodeTest.js
@@ -104,7 +104,7 @@ const showToFriends = function (phone) {
     return Promise.resolve(message);
 }
 
-
+fu
 
 
 
@@ -113,6 +113,38 @@ const showToFriends = function (phone) {
 */
 
 
+
+function Car() {
+    const [car,setCar] = useState({
+        brand:"Ford";
+         model: "Mustang",
+    year: "1964",
+    color: "red"
+    }) 
+
+const updateColor= ( ) => {
+    setCar(previousState=> {
+    return {...previousState,color:"blue"} 
+})
+
+return (
+    <>
+    <h1>My {car.brand}</h1>
+    <p>
+        it is a {car.color} {car.model} from {car.year}
+    </p>
+
+    <button type="button" onClick={updateColor}>
+            Blue
+    </button>
+
+    </>
+)
+
+
+
+
+}
 
 
 
