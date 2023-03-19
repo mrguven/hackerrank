47783e8142efac4a22958b9065d2e69ef29493c5
mrguven <mrguven@gmail.com>
2023-03-19 23:01:37 +0100

    update

--- life.js
+++ life.js
@@ -2,7 +2,14 @@
 let buttonSingUp = document.getElementById("singUp");
 buttonSingUp.addEventListener("click",()=> {window.location.href = "https://www.nos.nl"});
 
+let userName= document.getElementById("userName");
+let password= document.getElementById("password");
 
+
+
+
+
+if (userName===)
 
 
 
